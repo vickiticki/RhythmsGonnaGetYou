e3c9b5778ce4cb363f294924eaf64817d9791bb7
Victoria Tiller <vijotiller@gmail.com>
2021-04-30 09:14:51 -0400

    bands class started

--- Program.cs
+++ Program.cs
@@ -2,11 +2,23 @@
 
 namespace RhythmsGonnaGetYou
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Band
     {
-      Console.WriteLine("Welcome to C#");
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public int NumberOfMembers { get; set; }
+        public string Website { get; set; }
+        public string Style { get; set; }
+        public bool IsSigned { get; set; }
+        public string ContactName { get; set; }
+        public long ContactPhoneNumber { get; set; }
     }
-  }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+        }
+    }
 }
